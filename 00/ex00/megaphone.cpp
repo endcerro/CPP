6d6aa868f6210b9885acce86ed2d5e4096516aa9
hehe <haha>
2020-11-14 20:38:41 +0100

    phew

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 #include <string>
 
-class Contact {
-	std::string first_name;
-	std::string last_name;
-	std::string nickname;
-	std::string login;
-	int postal ;
-	std::string address;
-	std::string email_address;
-	std::string phone_number;
-	std::string birthday_date;
-	std::string favoritemeal;
-	std::string underwear_color;
-	std::string darkest_secret
-}
-
-
 int main(int argc, char const *argv[])
 {
+	std::string s;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";		
+	for (int i = 1; i < argc; i++)
+	{
+		s = argv[i];
+		for ( std::string::iterator it=s.begin(); it != s.end(); ++it)
+    		std::cout << (char)((*it >= 'a' && *it <= 'z') ? *it - 32 : *it);
+	}
+	std::cout << '\n';
 	return 0;
 }
