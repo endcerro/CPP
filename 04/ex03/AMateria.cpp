8764053aab45878ea0d3d93bf6943a75192559e7
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-03-31 16:15:12 +0200

    04 03 ok

--- 04/ex03/AMateria.cpp
+++ 04/ex03/AMateria.cpp
@@ -6,21 +6,22 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 12:39:10 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/03/30 18:10:27 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/03/31 16:14:44 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
 
-AMateria::AMateria(std::string const &type) : _type(type), _xp(0){}
+AMateria::AMateria(std::string const &type) : _xp(0), _type(type){}
 
-AMateria::AMateria(const AMateria &c)  : _type(c._type), _xp(c._xp)
+AMateria::AMateria(const AMateria &c)  : _xp(c._xp), _type(c._type) 
 {}
 
 AMateria::~AMateria() {}
 
 void AMateria::use(ICharacter &target)
 {
+	(void)(target);
 	_xp += 10;
 }
 
--- 04/ex03/main.cpp
+++ 04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 15:53:31 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/03/31 11:06:12 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/03/31 16:13:12 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,53 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 #include "MateriaSource.hpp"
-int main(int argc, char const *argv[])
+int main()
 {
-	Character enzo("Enzo");
+	// Character enzo("Enzo");
 
-	Character target("TGT");
+	// Character target("TGT");
 
-	Ice *i = new Ice;
-	Cure *c = new Cure;
+	// Ice *i = new Ice;
+	// Cure *c = new Cure;
 
-	Ice *ia = new Ice;
-	Cure *ca = new Cure;
+	// Ice *ia = new Ice;
+	// Cure *ca = new Cure;
 
-	Ice *ib = new Ice;
-	Cure *cb = new Cure;
+	// Ice *ib = new Ice;
+	// Cure *cb = new Cure;
 
-	// enzo.equip(i);
-	// enzo.equip(ia);
-	// enzo.equip(ib);
+	// // enzo.equip(i);
+	// // enzo.equip(ia);
+	// // enzo.equip(ib);
 	
-	// enzo.equip(c);
-	// enzo.equip(ca);
+	// // enzo.equip(c);
+	// // enzo.equip(ca);
 
-	// enzo.equip(cb);
+	// // enzo.equip(cb);
 
 
-	enzo.use(0, target);
-	enzo.use(1, target);
-	enzo.use(2, target);
-	enzo.use(3, target);
-	enzo.use(4, target);
-	enzo.use(5, target);
-	enzo.use(6, target);
+	// enzo.use(0, target);
+	// enzo.use(1, target);
+	// enzo.use(2, target);
+	// enzo.use(3, target);
+	// enzo.use(4, target);
+	// enzo.use(5, target);
+	// enzo.use(6, target);
 
-	MateriaSource m;
-	m.learnMateria(i);
-	m.learnMateria(c);
+	// MateriaSource m;
+	// m.learnMateria(i);
+	// m.learnMateria(c);
 
-	enzo.equip(m.createMateria("ice"));
-	enzo.equip(m.createMateria("ice"));
-	enzo.equip(m.createMateria("ice"));
-	enzo.equip(m.createMateria("cure"));
-	enzo.use(0, target);
-	enzo.use(1, target);
-	enzo.use(2, target);
-	enzo.use(3, target);
+	// enzo.equip(m.createMateria("ice"));
+	// enzo.equip(m.createMateria("ice"));
+	// enzo.equip(m.createMateria("ice"));
+	// enzo.equip(m.createMateria("cure"));
+	// enzo.use(0, target);
+	// enzo.use(1, target);
+	// enzo.use(2, target);
+	// enzo.use(3, target);
 	
 	// enzo.unequip(1);
-	
 	// enzo.use(0, target);
 	// enzo.use(1, target);
 	// enzo.use(2, target);
