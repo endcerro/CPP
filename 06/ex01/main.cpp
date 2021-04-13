988908721a43ec504bf15bb51cbe60c90dffb6f6
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-04-13 17:08:59 +0200

    06 01

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -6,32 +6,79 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 14:52:46 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/04/09 16:31:36 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/04/13 17:08:35 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 
-void fillstr(char *t)
-{
+#include <time.h>
+#include <iostream>
 
+struct Data { std::string s1; int n; std::string s2; };
+
+
+char randchar()
+{
+	
+	std::string test = "abcdefghijlmnopqrstuvwxyz";
+	char t = test[rand() % 26];
+	// std::cout << t;
+	return t ;
 }
 
 void *serialize(void) //Size of char is 1, int is 8
 {
-	void *test = (char *)malloc(sizeof(std::string *) * 2 + sizeof(int));
-	std::string *s1 = new std::string("Hello");
-	std::string *s2 = new std::string("World");
+	srand(time(NULL));
+	char *charv = new char[20];
+	int *intv = reinterpret_cast<int *>(charv);
+	char c;
+
+	std::cout << "S1 build : ";
 	
-	fillstr((char *)test)
+	for (int i = 0; i < 8; i++)
+	{
+		c = randchar();
+		std::cout << c;
+		charv[i] = c;
+	}
+	std::cout << std::endl;
+	
+	intv[2] = 420;
+	std::cout << "Int is : " << intv[2] <<std::endl;
+	std::cout << "S2 build : ";
+	for (int i = 12; i < 20; i++)
+	{
+		c = randchar();
+		std::cout << c;
+		charv[i] = c;
+	}
+	std::cout << std::endl;
+	return charv;
+}
 
-	// test << s1 << s2;
-	// 0*test = s1;
 
-	return 0;
+Data *deserialize(void *raw)
+{
+	Data *d = new Data();
+	char *charv = reinterpret_cast<char *>(raw);
+	int *intv = reinterpret_cast<int *>(raw);
+
+	for (int i = 0; i < 8; i++)
+		d->s1 += charv[i];
+	d->n = intv[2];
+	for (int i = 12; i < 20; i++)
+		d->s2 += charv[i];
+	
+	return d;
 }
 
 int main()
 {
+	void *raw = serialize();
+	Data *d = deserialize(raw);
+	std::cout << "S1 = " << d->s1 << " int = " << d->n << " S2= " << d->s2 << std::endl;
+	delete static_cast<char *>(raw);
+	delete d;
 	return 0;
 }
