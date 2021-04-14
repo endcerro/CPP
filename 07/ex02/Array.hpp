1def6270539f1ad71cd3a381d7d5efbb79862c06
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-04-14 15:26:17 +0200

    07 02

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -6,17 +6,68 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 18:18:45 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/04/13 18:20:49 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/04/14 15:25:39 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+#include <stdexcept>
+#include <iostream>
 template<typename T>
-void array(void)
-{
+class Array {
 	
-}
+	public :
+	
+		Array() : _size(0)
+		{
+			_array = 0;
+		}
+		Array(unsigned int t) : _size(t)
+		{
+			_array = new T[t];
+		}
+		
+		~Array()
+		{
+			delete _array;
+			_array = 0;
+		}
+		int size() const
+		{
+			return _size;
+		}
+		Array(const Array &a) : _size(a._size)
+		{
+			_array = new T[a._size];
+			for (int i = 0; i < _size; i++)
+				_array[i] = a._array[i];
+		}
+		Array& operator=(Array<T> a)
+		{
+			T *tmp = a._array;
+			a._array = this->_array;
+			this->_array = tmp;
+			this->_size = a._size;
+			return *this;
+		}
+		T& operator[](int p)
+		{
+			if (p < 0 || p >= _size)
+				throw std::out_of_range("Out of bounds");
+			return _array[p];
+		}
+		void print()
+		{
+			for (int i = 0; i < _size; i++)
+				std::cout << _array[i] << " ";
+			std::cout << std::endl;
+		}
+	private :
+		int _size;
+		T *_array;
+};
 
 #endif
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -6,12 +6,42 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 18:18:20 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/04/13 18:19:29 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/04/14 15:18:33 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+#include <iostream>
+
 int main()
 {
-	/* code */
+	Array<int> ar(5);
+	Array<int> ar2(10);
+
+	
+	for (int i = 0; i < 10; i++)
+	{
+		ar2[i] = i;
+	// 	std::cout << i << " ";
+	}
+	// std::cout << std::endl;
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		ar[i] = i * 5;
+		// std::cout << ar2[i] << " ";
+	}
+	// std::cout << std::endl;
+	std::cout << "Arr 1" << std::endl;
+	ar.print();	
+	std::cout << "Arr 2" << std::endl;
+	ar2.print();
+	ar = ar2;
+	std::cout << "Arr 1" << std::endl;
+	ar.print();	
+	std::cout << "Arr 2" << std::endl;
+	ar2.print();
 	return 0;
 }
