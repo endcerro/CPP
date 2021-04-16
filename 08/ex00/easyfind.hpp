8d778ead7b083d809ad2fe4515c88e2e4901e0c3
Enzo Dal Cerro <enzodalcerro@gmail.com>
2021-04-16 15:36:59 +0200

    ok

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edal <edal@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 15:29:33 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/04/14 17:35:38 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/04/16 15:10:37 by edal             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <exception>
 
 template<typename T>
 typename T::iterator easyfind(T& cont, int n)
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edal <edal@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 15:29:44 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/04/14 17:35:13 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/04/16 15:34:36 by edal             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,71 @@
 #include <list>
 int main()
 {	
-	// int arr[5] 
-	std::list<int> v;// = {1,2,3,4,5};
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
+	// int arr[5];
+	std::list<int> l;
+	l.push_back(1);
+	l.push_back(2);
+	l.push_back(3);
+	l.push_back(5);
 
-	easyfind(v, 4);
-	// std::cout << ret << std::endl;
+	int key = 120;
+
+	std::cout << "Trying to find " << key << " : ";
+	try 
+	{
+		std::list<int>::iterator i = easyfind(l, 4);
+		std::cout << "OK " << *i << std::endl;
+
+	}
+	catch(std::exception &e)
+	{
+		std::cout << "KO  " << e.what() << std::endl;
+	}
+	key = 5;
+
+	std::cout << "Trying to find " << key << " : ";
+	try 
+	{
+		std::list<int>::iterator i = easyfind(l, key);
+		std::cout << "OK " << *i << std::endl;
+
+	}
+	catch(std::exception &e)
+	{
+		std::cout << "KO  " << e.what() << std::endl;
+	}
+
+	std::vector<int> v(10, 0);
+	v[0] = key = 150;
+	v.push_back(3);
+	v.push_back(2);
+	v.push_back(4);
+	
+
+	std::cout << "Trying to find " << key << " : ";
+	try 
+	{
+		std::vector<int>::iterator i = easyfind(v, 150);
+		std::cout << "OK " << *i << std::endl;
+
+	}
+	catch(std::exception &e)
+	{
+		std::cout << "KO  " << e.what() << std::endl;
+	}
+	key = 5;
+
+	std::cout << "Trying to find " << key << " : ";
+	try 
+	{
+		std::vector<int>::iterator i = easyfind(v, key);
+		std::cout << "OK " << *i << std::endl;
+
+	}
+	catch(std::exception &e)
+	{
+		std::cout << "KO  " << e.what() << std::endl;
+	}
+
 	return 0;
 }
