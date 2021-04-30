69f1a23c4007f985909a1046a11703889144db3a
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-04-30 16:09:02 +0200

    08 done

--- 08/ex02/mutantstack.hpp
+++ 08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 14:35:01 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/04/30 14:55:23 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/04/30 16:06:09 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,24 @@ class MutantStack : public std::stack<T>
 {
 	public:
 
-		MutantStack();
-		~MutantStack();
+		MutantStack() : std::stack<T>() {};
+		~MutantStack() {} ;
 
-
-		// MutantStack(const MutantStack& copy) : std::stack<T>(copy) {};
-		// MutantStack<T>& operator=(const MutantStack<T>& assign)
-		// 		{std::stack<T>::operator=(assign);return (*this);};
-
-		// For creating in the main an iterator object and for the return value of begin / end methods
-		// typedef typename std::stack<T>::container_type::iterator iterator;
-
-		// Begin and end methods that returns an iterator of the underlying container object
-		// that stack is using (could be deque, vector or list. Is a deque by default).
-		// Std::stack<T>::c is a member object of stack container that is the the underlying container.
-		// iterator begin() { return (std::stack<T>::c.begin()); }
-		// iterator end() { return (std::stack<T>::c.end()); }
+		MutantStack(const MutantStack& copy) : std::stack<T>(copy) {};
+		MutantStack<T>& operator=(const MutantStack<T>& m)
+		{
+			std::stack<T>::operator=(m);
+			return (*this);
+		};
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() 
+		{ 
+			return (std::stack<T>::c.begin()); 
+		}
+		iterator end() 
+		{ 
+			return (std::stack<T>::c.end()); 
+		}
 };
 
 #endif
